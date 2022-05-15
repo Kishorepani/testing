35e20f2c143921d09ad03d9bac27c15837ce2383
Kishore Kumar Pani <105485341+Kishorepani@users.noreply.github.com>
2022-05-15 10:29:41 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,7 +1,9 @@
 #I will implement this code
 def add(x,y):
+  #if a and y are interger than add them
   if(type(x) == int and type(y)==int):
 return x+y
+#otherwise do nothing
 return None
 
 #xyz will implement this code
